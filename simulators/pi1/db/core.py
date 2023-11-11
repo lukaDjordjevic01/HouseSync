184cacdcc3dd99a3fbf54fa9bf80a988f7413304
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2023-11-11 12:37:24 +0100

    #5 Updated buzzer to be without an input

--- simulators/pi1/db/core.py
+++ simulators/pi1/db/core.py
@@ -1,16 +1,26 @@
-#import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
+import keyboard
 
 
 def run(device_id, settings):
     # GPIO.setmode(GPIO.BCM)
     # GPIO.setup(settings['pin'], GPIO.OUT)
-    while True:
-        option = input("Buzz the buzzer with 'b' and exit this simulation with 'x': ")
-        if option == 'b':
+    print("Buzz the buzzer with 'b' and exit this simulation with 'x'")
+    exit_flag = [False]
+
+    def on_key_event(e):
+        if e.name == 'x' and e.event_type == keyboard.KEY_DOWN:
+            exit_flag[0] = True
+        elif e.name == 'b' and e.event_type == keyboard.KEY_DOWN:
             buzz(device_id, settings['pin'])
-        else:
-            return
+
+    keyboard.hook(on_key_event)
+
+    while not exit_flag[0]:
+        time.sleep(0.1)
+
+    keyboard.unhook_all()
 
 
 def buzz(device_id, pin, pitch=440, duration=1):
@@ -18,9 +28,9 @@ def buzz(device_id, pin, pitch=440, duration=1):
     delay = period / 2
     cycles = int(duration * pitch)
     for i in range(cycles):
-        #GPIO.output(pin, True)
+        # GPIO.output(pin, True)
         time.sleep(delay)
-        #GPIO.output(pin, False)
+        # GPIO.output(pin, False)
         time.sleep(delay)
     print(f"{device_id} buzzed")
     time.sleep(1)
