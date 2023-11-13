8b09c02dc92973ec513702a29b34daec701f8b57
Luka Djordjevic <lukadjordjevicsm01@gmail.com>
2023-11-13 09:41:25 +0100

    Minor update

--- simulators/pi1/db/core.py
+++ simulators/pi1/db/core.py
@@ -1,11 +1,11 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 import keyboard
 
 
 def run(device_id, settings):
-    # GPIO.setmode(GPIO.BCM)
-    # GPIO.setup(settings['pin'], GPIO.OUT)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(settings['pin'], GPIO.OUT)
     print("Buzz the buzzer with 'b' and exit this simulation with 'x'")
     exit_flag = [False]
 
@@ -28,9 +28,9 @@ def buzz(device_id, pin, pitch=440, duration=1):
     delay = period / 2
     cycles = int(duration * pitch)
     for i in range(cycles):
-        # GPIO.output(pin, True)
+        GPIO.output(pin, True)
         time.sleep(delay)
-        # GPIO.output(pin, False)
+        GPIO.output(pin, False)
         time.sleep(delay)
     print(f"{device_id} buzzed")
     time.sleep(1)
