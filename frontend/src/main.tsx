36b1fe2e9c5511d622a085ded62a7b6e35a75bcb
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2023-12-29 15:33:27 +0100

    Initial frontend layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,55 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import Navbar from "./components/navbar.tsx";
+import Alarms from "./pages/alarms.tsx";
+import Devices from "./pages/devices.tsx";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#4D4D4D",
+            light: "#174384",
+            contrastText: "#F2F2F2"
+        },
+        secondary: {
+            main: "#F5F5F5",
+            contrastText: '#4D4D4D'
+        },
+        error: {
+            main: "#DD3636"
+        },
+        success: {
+            main: "#66FF77"
+        },
+        background: {
+            default: "#1E1E1E",
+        }
+    },
+    typography: {
+        allVariants: {
+            color: "#F2F2F2",
+            fontFamily: "Poppins"
+        },
+
+    },
+});
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <Navbar/>,
+        children:[
+            {path: "/alarms", element: <Alarms/>},
+            {path: "/devices", element: <Devices/>}
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme} >
+        <CssBaseline/>
+        <RouterProvider router={router}/>
+    </ThemeProvider>
 )
---
+++ frontend/src/pages/devices.tsx
@@ -0,0 +1,8 @@
+
+export default function Devices () {
+    return(
+        <div>
+            <h1>Devices work</h1>
+        </div>
+    )
+}
