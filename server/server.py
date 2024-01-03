e2a96e732f5e1db859ba80a2775eb4f0e7d977bc
cuturic01 <miloscuturic8@gmail.com>
2024-01-03 17:56:19 +0100

    #38 Configured web sockets

--- server/server.py
+++ server/server.py
@@ -6,8 +6,10 @@ import paho.mqtt.client as mqtt
 import json
 from communication_credentials import *
 from simulators.settings.settings import load_settings
+from flask_socketio import SocketIO, join_room, leave_room, send
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="http://localhost:5173")
 CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
 influxdb_client = InfluxDBClient(url=influx_url, token=influx_token, org=influx_org)
@@ -51,6 +53,29 @@ def save_to_db(data):
         .field("measurement", data["value"])
     )
     write_api.write(bucket=influx_bucket, org=influx_org, record=point)
+    socketio.emit('message', {'topic': data["id"], 'message': data}, room=data["id"])
+
+
+@socketio.on('subscribe')
+def handle_subscribe(data):
+    topic = data['topic']
+    print(f'Client subscribed to topic: {topic}')
+    join_room(topic)
+
+
+@socketio.on('unsubscribe')
+def handle_unsubscribe(data):
+    topic = data['topic']
+    print(f'Client unsubscribed from topic: {topic}')
+    leave_room(topic)
+
+
+@socketio.on('send_message')
+def handle_send_message(data):
+    topic = data['topic']
+    message = data['message']
+    # print(f'Received message on topic {topic}: {message}')
+    socketio.emit('message', {'topic': topic, 'message': message}, room=topic)
 
 
 @app.route('/get-devices', methods=['get'])
@@ -65,4 +90,4 @@ def get_devices():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    socketio.run(app, debug=False, allow_unsafe_werkzeug=True)
